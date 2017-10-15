f1a195fbd4441cea42552b55f4d0a4eb2d8e96b3
Shubhodeep Mitra <shubhodeep.mitra00@gmail.com>
2017-10-15 19:06:34 +0530

    completed mini project for Decision Trees

--- Udacity: Intro to Machine Learning/decision_tree/dt_author_id.py
+++ Udacity: Intro to Machine Learning/decision_tree/dt_author_id.py
@@ -1,18 +1,23 @@
 #!/usr/bin/python
 
-""" 
+"""
+    Shubhodeep Mitra
+    15/10/2017
+
     This is the code to accompany the Lesson 3 (decision tree) mini-project.
 
-    Use a Decision Tree to identify emails from the Enron corpus by author:    
+    Use a Decision Tree to identify emails from the Enron corpus by author:
     Sara has label 0
     Chris has label 1
 """
-    
+
 import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
 
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
@@ -26,6 +31,26 @@ features_train, features_test, labels_train, labels_test = preprocess()
 ### your code goes here ###
 
 
+
+t_ini=time()
+clf=DecisionTreeClassifier(min_samples_split=40)
+clf.fit(features_train,labels_train)
+
+t_train=time()
+print "time taken to train:",str(t_train-t_ini)
+pred=clf.predict(features_test)
+
+t_pred=time()
+print "time taken to predict:",str(t_pred-t_train)
+
+acc=accuracy_score(labels_test,pred)
+print "accuracy is:",str(acc)
+
+
+''' Task: To print the number of features(columns) in the training set
+'''
+
+print 'Number of features:',len(features_train[0]),
+
+
 #########################################################
-
-
