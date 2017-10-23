c6ca1b2c0ba0d63f07b6768aa76ba2c35e72edce
Shubhodeep Mitra <shubhodeep.mitra00@gmail.com>
2017-10-24 00:28:48 +0530

    Completed Lesson 7:Regression

--- Udacity: Intro to Machine Learning/regression/finance_regression.py
+++ Udacity: Intro to Machine Learning/regression/finance_regression.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python
 
 """
+    Shubhodeep Mitra
+    23/10/2017
+
     Starter code for the regression mini-project.
-    
+
     Loads up/formats a modified version of the dataset
     (why modified?  we've removed some trouble points
     that you'll find yourself in the outliers mini-project).
@@ -10,18 +13,24 @@
     Draws a little scatterplot of the training/testing data
 
     You fill in the regression code where indicated:
-"""    
+"""
 
 
 import sys
 import pickle
+from time import time
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
 dictionary = pickle.load( open("../final_project/final_project_dataset_modified.pkl", "r") )
 
-### list the features you want to look at--first item in the 
-### list will be the "target" feature
+### list the features you want to look at--first item in the
+### list will be the "target" feature,  regression the bonus against salary score:-1.48499241737
 features_list = ["bonus", "salary"]
+
+## task: use long_term_incentive as input and perform regression to find the score: -0.59271289995
+##features_list = ["bonus","long_term_incentive"]
+
+
 data = featureFormat( dictionary, features_list, remove_any_zeroes=True)
 target, features = targetFeatureSplit( data )
 
@@ -29,16 +38,37 @@ target, features = targetFeatureSplit( data )
 from sklearn.cross_validation import train_test_split
 feature_train, feature_test, target_train, target_test = train_test_split(features, target, test_size=0.5, random_state=42)
 train_color = "b"
-test_color = "b"
+test_color = "r"
 
 
 
 ### Your regression goes here!
-### Please name it reg, so that the plotting code below picks it up and 
+### Please name it reg, so that the plotting code below picks it up and
 ### plots it correctly. Don't forget to change the test_color above from "b" to
 ### "r" to differentiate training points from test points.
 
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
 
+reg=LinearRegression()
+
+ini_t=time()
+reg.fit(feature_train,target_train)
+
+train_t=time()
+print "training time:",str(train_t - ini_t)
+
+pred= reg.predict(feature_test)
+pred_t=time()
+print "prediction time:",str(pred_t - train_t)
+
+print "slope:",reg.coef_   #to get the slope
+print "intercept:",reg.intercept_  #to get the intercept
+
+
+print "training accuracy:", reg.score(feature_train,target_train)
+print "prediction accuracy", reg.score(feature_test,target_test)
+print "r squared score",r2_score(target_test,pred)
 
 
 
@@ -48,9 +78,9 @@ test_color = "b"
 ### draw the scatterplot, with color-coded training and testing points
 import matplotlib.pyplot as plt
 for feature, target in zip(feature_test, target_test):
-    plt.scatter( feature, target, color=test_color ) 
+    plt.scatter( feature, target, color=test_color )
 for feature, target in zip(feature_train, target_train):
-    plt.scatter( feature, target, color=train_color ) 
+    plt.scatter( feature, target, color=train_color )
 
 ### labels for the legend
 plt.scatter(feature_test[0], target_test[0], color=test_color, label="test")
@@ -64,6 +94,11 @@ try:
     plt.plot( feature_test, reg.predict(feature_test) )
 except NameError:
     pass
+
+## task: to check how outliers effect regression
+reg.fit(feature_test, target_test)
+plt.plot(feature_train, reg.predict(feature_train), color="g")
+
 plt.xlabel(features_list[1])
 plt.ylabel(features_list[0])
 plt.legend()
