a7c3e5d17d0e8b4b33ea0d8cb212508fe5402fa3
Shubhodeep Mitra <shubhodeep.mitra00@gmail.com>
2017-10-26 20:43:10 +0530

    Lesson 9:Clustering complete

--- Udacity: Intro to Machine Learning/k_means/k_means_cluster.py
+++ Udacity: Intro to Machine Learning/k_means/k_means_cluster.py
@@ -1,6 +1,9 @@
-#!/usr/bin/python 
+#!/usr/bin/python
 
-""" 
+"""
+    Shubhodeep Mitra
+    25/10/2017
+
     Skeleton code for k-means clustering mini-project.
 """
 
@@ -40,30 +43,56 @@ def Draw(pred, features, poi, mark_poi=False, name="image.png", f1_name="feature
 
 ### load in the dict of dicts containing all the data on each person in the dataset
 data_dict = pickle.load( open("../final_project/final_project_dataset.pkl", "r") )
-### there's an outlier--remove it! 
+### there's an outlier--remove it!
 data_dict.pop("TOTAL", 0)
 
 
-### the input features we want to use 
-### can be any key in the person-level dictionary (salary, director_fees, etc.) 
+### the input features we want to use
+### can be any key in the person-level dictionary (salary, director_fees, etc.)
 feature_1 = "salary"
 feature_2 = "exercised_stock_options"
+feature_3="total_payments"  ## adding a new feature
 poi  = "poi"
-features_list = [poi, feature_1, feature_2]
+features_list = [poi, feature_1, feature_2,feature_3]
 data = featureFormat(data_dict, features_list )
 poi, finance_features = targetFeatureSplit( data )
 
 
 ### in the "clustering with 3 features" part of the mini-project,
-### you'll want to change this line to 
+### you'll want to change this line to
 ### for f1, f2, _ in finance_features:
 ### (as it's currently written, the line below assumes 2 features)
-for f1, f2 in finance_features:
-    plt.scatter( f1, f2 )
+
+f22=[]
+f11=[]
+for f1, f2,f3 in finance_features:
+    plt.scatter( f1, f2, ) ## we can only show 2D plots hence only two features are being plotted
+    f22.append([f2])
+    f11.append([f1])
 plt.show()
+
+
 
 ### cluster here; create predictions of the cluster labels
 ### for the data and store them to a list called pred
+from sklearn.cluster import KMeans
+
+kmeans=KMeans(n_clusters=2)
+kmeans.fit(finance_features)
+pred=kmeans.predict(finance_features)
+
+'''
+## task: print the max and min in the list exercised_stock_options
+print "feature_2:",sorted(f22)
+print "min:",min(f22)
+print "max:",max(f22)
+
+print "feature_1",sorted(f11)
+'''
+
+
+
+
 
 
 
